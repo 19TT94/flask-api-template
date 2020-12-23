884b28c670f7671f6ff0ef1d6ae61f0997808e16
19tt94 <19tt94@gmail.com>
2020-12-23 13:21:59 -0800

    added folders and files for database

---
+++ api/models/example.py
@@ -0,0 +1,15 @@
+# No database by Default this an example Database Model that will not work
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    username  = db.Column(db.String(80), unique=True, nullable=False)
+    email     = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
